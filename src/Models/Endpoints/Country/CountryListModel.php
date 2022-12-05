c14de0a31ea4886f6a94a8a17f7541f7e058d3cf
Hendrik Mennen <77828605+hmennen90@users.noreply.github.com>
2022-12-05 23:03:02 +0100

    Added:
    Response Model for Country Facade

---
+++ src/Models/Endpoints/Country/CountryListModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MennenOnline\Shopware5ApiConnector\Models\Endpoints\Country;
+
+use MennenOnline\LaravelResponseModels\Models\BaseModel;
+
+class CountryListModel extends BaseModel
+{
+
+}
---
+++ tests/Feature/Endpoints/CountryEndpointTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace MennenOnline\Shopware5ApiConnector\Tests\Feature\Endpoints;
+
+use Illuminate\Support\Facades\Http;
+use MennenOnline\Shopware5ApiConnector\Facades\CategoryFacade;
+use MennenOnline\Shopware5ApiConnector\Facades\CountryFacade;
+use MennenOnline\Shopware5ApiConnector\Models\BaseResponseModel;
+use MennenOnline\Shopware5ApiConnector\Models\Endpoints\Category\CategoryListModel;
+use MennenOnline\Shopware5ApiConnector\Models\Endpoints\Category\CategorySingleModel;
+use MennenOnline\Shopware5ApiConnector\Models\Endpoints\Country\CountryListModel;
+use MennenOnline\Shopware5ApiConnector\Models\Endpoints\Country\CountrySingleModel;
+use MennenOnline\Shopware5ApiConnector\Tests\BaseTest;
+
+class CountryEndpointTest extends BaseTest
+{
+    /**
+     * @test
+     */
+    public function it_can_retrieve_a_list_of_countries() {
+        $response = file_get_contents('./Responses/Country/list.json');
+
+        Http::fake([
+            '*' => Http::response((array)json_decode($response))
+        ]);
+
+        $result = CountryFacade::initialize('http://localhost', 'test', 'test')->getAll();
+
+        $this->assertInstanceOf(BaseResponseModel::class, $result);
+
+        $this->assertInstanceOf(CountryListModel::class, $result->data->first());
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_retrieve_a_single_country() {
+        $response = file_get_contents('./Responses/Country/single.json');
+
+        Http::fake([
+            '*' => Http::response((array)json_decode($response))
+        ]);
+
+        $result = CountryFacade::initialize('http://localhost', 'test', 'test')->getSingle(1);
+
+        $this->assertInstanceOf(BaseResponseModel::class, $result);
+
+        $this->assertInstanceOf(CountrySingleModel::class, $result->data);
+    }
+}
