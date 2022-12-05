8020ab13af97731eb688d43bbb5e5430b9dfc84d
Hendrik Mennen <77828605+hmennen90@users.noreply.github.com>
2022-12-05 22:55:07 +0100

    Added:
    Response Model for Category Facade

---
+++ tests/Feature/Endpoints/CategoryEndpointTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace MennenOnline\Shopware5ApiConnector\Tests\Feature\Endpoints;
+
+use Illuminate\Support\Facades\Http;
+use MennenOnline\Shopware5ApiConnector\Facades\CacheFacade;
+use MennenOnline\Shopware5ApiConnector\Facades\CategoryFacade;
+use MennenOnline\Shopware5ApiConnector\Models\BaseResponseModel;
+use MennenOnline\Shopware5ApiConnector\Models\Endpoints\Cache\CacheListModel;
+use MennenOnline\Shopware5ApiConnector\Models\Endpoints\Cache\CacheSingleModel;
+use MennenOnline\Shopware5ApiConnector\Models\Endpoints\Category\CategoryListModel;
+use MennenOnline\Shopware5ApiConnector\Models\Endpoints\Category\CategorySingleModel;
+use MennenOnline\Shopware5ApiConnector\Tests\BaseTest;
+
+class CategoryEndpointTest extends BaseTest
+{
+    /**
+     * @test
+     */
+    public function it_can_retrieve_a_list_of_categories() {
+        $response = file_get_contents('./Responses/Category/list.json');
+
+        Http::fake([
+            '*' => Http::response((array)json_decode($response))
+        ]);
+
+        $result = CategoryFacade::initialize('http://localhost', 'test', 'test')->getAll();
+
+        $this->assertInstanceOf(BaseResponseModel::class, $result);
+
+        $this->assertInstanceOf(CategoryListModel::class, $result->data->first());
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_retrieve_a_single_category() {
+        $response = file_get_contents('./Responses/Category/single.json');
+
+        Http::fake([
+            '*' => Http::response((array)json_decode($response))
+        ]);
+
+        $result = CategoryFacade::initialize('http://localhost', 'test', 'test')->getSingle(1);
+
+        $this->assertInstanceOf(BaseResponseModel::class, $result);
+
+        $this->assertInstanceOf(CategorySingleModel::class, $result->data);
+    }
+}
