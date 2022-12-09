d4b744c662846937ac354b5322fd525b444094b2
Hendrik Mennen <77828605+hmennen90@users.noreply.github.com>
2022-12-09 09:18:11 +0100

    Added:
    Response Model for Property Group Facade

---
+++ src/Models/Endpoints/PropertyGroup/PropertyGroupListModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MennenOnline\Shopware5ApiConnector\Models\Endpoints\PropertyGroup;
+
+use MennenOnline\LaravelResponseModels\Models\BaseModel;
+
+class PropertyGroupListModel extends BaseModel
+{
+
+}
---
+++ tests/Feature/Endpoints/PropertyGroupEndpointTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace MennenOnline\Shopware5ApiConnector\Tests\Feature\Endpoints;
+
+use Illuminate\Support\Facades\Http;
+use MennenOnline\Shopware5ApiConnector\Facades\OrderFacade;
+use MennenOnline\Shopware5ApiConnector\Facades\PaymentMethodFacade;
+use MennenOnline\Shopware5ApiConnector\Facades\PropertyGroupFacade;
+use MennenOnline\Shopware5ApiConnector\Models\BaseResponseModel;
+use MennenOnline\Shopware5ApiConnector\Models\Endpoints\Order\OrderListModel;
+use MennenOnline\Shopware5ApiConnector\Models\Endpoints\Order\OrderSingleModel;
+use MennenOnline\Shopware5ApiConnector\Models\Endpoints\PaymentMethod\PaymentMethodListModel;
+use MennenOnline\Shopware5ApiConnector\Models\Endpoints\PaymentMethod\PaymentMethodSingleModel;
+use MennenOnline\Shopware5ApiConnector\Models\Endpoints\PropertyGroup\PropertyGroupListModel;
+use MennenOnline\Shopware5ApiConnector\Models\Endpoints\PropertyGroup\PropertyGroupSingleModel;
+use MennenOnline\Shopware5ApiConnector\Tests\BaseTest;
+
+class PropertyGroupEndpointTest extends BaseTest
+{
+    /**
+     * @test
+     */
+    public function it_can_retrieve_a_list_of_property_groups() {
+        $response = file_get_contents(__DIR__.'/Responses/PropertyGroup/list.json');
+
+        Http::fake([
+            '*' => Http::response((array)json_decode($response))
+        ]);
+
+        $result = PropertyGroupFacade::initialize('http://localhost', 'test', 'test')->getAll();
+
+        $this->assertInstanceOf(BaseResponseModel::class, $result);
+
+        $this->assertInstanceOf(PropertyGroupListModel::class, $result->data->first());
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_retrieve_a_single_property_group() {
+        $response = file_get_contents(__DIR__.'/Responses/PropertyGroup/single.json');
+
+        Http::fake([
+            '*' => Http::response((array)json_decode($response))
+        ]);
+
+        $result = PropertyGroupFacade::initialize('http://localhost', 'test', 'test')->getSingle(1);
+
+        $this->assertInstanceOf(BaseResponseModel::class, $result);
+
+        $this->assertInstanceOf(PropertyGroupSingleModel::class, $result->data);
+    }
+}
