a4de9a35246a66d511bc31d72bb4f0b715e4d0df
Hendrik Mennen <77828605+hmennen90@users.noreply.github.com>
2022-12-09 09:35:20 +0100

    Added:
    Response Model for Variant Facade

--- src/Endpoints/VariantEndpoint.php
+++ src/Endpoints/VariantEndpoint.php
@@ -3,16 +3,29 @@
 namespace MennenOnline\Shopware5ApiConnector\Endpoints;
 
 use MennenOnline\Shopware5ApiConnector\Enums\EndpointEnum;
+use MennenOnline\Shopware5ApiConnector\Models\BaseResponseModel;
+use MennenOnline\Shopware5ApiConnector\Models\Endpoints\Variant\VariantListModel;
+use MennenOnline\Shopware5ApiConnector\Models\Endpoints\Variant\VariantSingleModel;
 use MennenOnline\Shopware5ApiConnector\Shopware5ApiConnector;
 
-class VariantEndpoint extends Shopware5ApiConnector
+class VariantEndpoint extends Endpoint
 {
-    public function initialize(?string $url = null, ?string $username = null, ?string $password = null, ?string $model = null): Shopware5ApiConnector
+    public function initialize(?string $url = null, ?string $username = null, ?string $password = null): Shopware5ApiConnector
     {
-        $this->responseModel = $model;
-
         $this->endpoint = EndpointEnum::VARIANTS;
 
         return parent::initialize($url, $username, $password);
+    }
+
+    public function getAll(?int $limit = null): BaseResponseModel {
+        $this->responseModel = VariantListModel::class;
+
+        return parent::getAll($limit);
+    }
+
+    public function getSingle(int|string $id): BaseResponseModel {
+        $this->responseModel = VariantSingleModel::class;
+
+        return parent::getSingle($id);
     }
 }
---
+++ src/Models/Endpoints/Variant/VariantSingleModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MennenOnline\Shopware5ApiConnector\Models\Endpoints\Variant;
+
+use MennenOnline\LaravelResponseModels\Models\BaseModel;
+
+class VariantSingleModel extends BaseModel
+{
+
+}
